800367e2d251d3d522fb2fee6fc5a9c591051a74
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-07-06 22:18:11 +0000

    validate

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+A module to be validated.
+"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple[int], factor: int = 2) -> List[int]:
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array: Tuple[int, int, int] = (12, 72, 91)
+
+zoom_2x: List[int] = zoom_array(array)
+
+zoom_3x: List[int] = zoom_array(array, 3)
